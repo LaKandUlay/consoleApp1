6c6ec1d0fd25212d03386b51bcb602dd246558be
Jacob Loste <ziggerford@gmail.com>
2025-09-26 13:41:38 +0800

    Initial commit for ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please Enter first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Please Enter last name");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("Hello, {0}, {1}", firstName, lastName);
+
+        string x = "Loste Jacob";
+        int y = 123;
+
+        Console.WriteLine("{0} and {1}", y, x);
+    }
+}
---
+++ DotNetProgram.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("Hello World");
+string x = "Loste Jacob";
+int y = 123;
+float t = 33.33;
+Console.WriteLine("{0} and {1}", y, t);
